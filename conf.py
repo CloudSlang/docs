93e32acf0175d50e6a2a2042724ef175c7a9d458
SamMarkowitz <sam.markowitz@hpe.com>
2015-12-01 11:17:30 +0200

    fixed to work everywhere
    Signed-off-by: SamMarkowitz <sam.markowitz@hpe.com>

--- conf.py
+++ conf.py
@@ -110,15 +110,19 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
-html_style = 'css/cloudslang_theme.css'
-html_context = {
-    'css_files': [
-        'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-        '_static/css/cloudslang_theme.css',
-    ],
-  }
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:
+    html_theme = 'sphinx_rtd_theme'
+    html_style = 'css/cloudslang_theme.css'
+else:
+    html_context = {
+        'css_files': [
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+            '_static/css/cloudslang_theme.css',
+        ],
+    }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
