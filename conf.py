5e9f44a6dbd0f0dcf9cf936d0ad9f8fe2141f155
SamMarkowitz <sam.markowitz@hpe.com>
2016-03-10 13:57:56 +0200

    try to fix colors
    Signed-off-by: SamMarkowitz <sam.markowitz@hpe.com>

--- conf.py
+++ conf.py
@@ -122,6 +122,9 @@ else:
             'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
             '_static/css/cloudslang_theme.css',
         ],
+        'extra_css_files' : [
+            '_static/css/cloudslang_theme.css'
+        ]
     }
 
 # Theme options are theme-specific and customize the look and feel of a theme
